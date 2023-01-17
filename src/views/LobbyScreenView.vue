5ba709727f1987202b651b5e859057743147980d
malik250danish <99634677+malik250danish@users.noreply.github.com>
2023-01-17 14:11:17 +0000

    bhfhfgh

--- src/views/LobbyScreenView.vue
+++ src/views/LobbyScreenView.vue
@@ -104,7 +104,7 @@ export default {
     border-style: solid;
 
     border-image: linear-gradient(to right, #EE2623, #F78E1E);
-    border-image-slice: 1;
+    border-image-slice: 1 !important;
     border-radius: 5px;
 
 }
