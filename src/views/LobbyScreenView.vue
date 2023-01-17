973870667ad3c97307c4ebb4dd3b425e98ba5e92
malik250danish <danish@250ahmad@gmail.com>
2023-01-17 18:20:33 +0500

    mew commit

--- src/views/LobbyScreenView.vue
+++ src/views/LobbyScreenView.vue
@@ -9,19 +9,19 @@
             <div class="row  justify-content-center">
                 <div class="col-12 col-md-6 col-lg-2 align-items-center d-flex flex-column tScreen">
                     <div class="screen mt-2 bounce-enter-active ">
-                        <h3 class="text-center">Heading</h3>
+                        <h3 class="text-center">การเดิมพันของคุณ</h3>
                     </div>
                     <div class="screen mt-2 ml-2 bounce-enter-active">
-                        <h3 class="text-center">Heading</h3>
+                        <h3 class="text-center">คู่มวยอื่น</h3>
                     </div>
                 </div>
                 <div class="col-12 col-md-12 col-lg-8 d-flex flex-column justify-content-center align-items-center   ">
                     <div class="vs mx-auto ">
-                        <img src="../assets/VS.png" width="70%" style="margin-left:100px">
+                        <img src="../assets/VS.png" width="70%" >
                         <!-- <h3 class="text-center text-white">VS</h3> -->
                     </div>
                     <div class=" screen2 bounce-enter-active mt-1">
-
+           
                     </div>
                   <div class="col-12">
                     <div class="row mt-4 justify-content-center punchRow">
@@ -34,7 +34,7 @@
                                     <button class="gBtn">Text</button>
                                 </div>
                             </div>
-                            <div class="col-12 mt-3 d-flex justify-content-center">
+                            <div class="col-12 mt-3 d-flex justify-content-centerg">
                                 <div class="row">
                                 <div class="col-12 col-md-6 col-lg-2 mb-2 d-flex justify-content-center">
                                     <button class="btnF">btn</button>
@@ -51,11 +51,11 @@
                                 </div>
                             </div>
                             <div class="d-flex justify-content-between">
-                                <button class="btnp">+10</button>
-                                <button class="btnp" >+10</button>
-                                <button class="btnp">+10</button>
-                                <button class="btnp">+10</button>
-                                <button class="btnp">+10</button>
+                                <button class="btnp">+ 10</button>
+                                <button class="btnp" >+ 50</button>
+                                <button class="btnp">+ 100</button>
+                                <button class="btnp">+ 500</button>
+                                <button class="btnp">+ 1000</button>
                             </div>
                         </div>
                         <div class="col-6 col-md-6 col-lg-2 mt-3 mb-2">
@@ -66,48 +66,13 @@
 
                 </div>
                 <div class="col-12 col-lg-2 col-md-6 s3 mt-1">
-                    <div class=" Screen3 bounce-enter-active ">
-                        <h3>Heading</h3>
+                    <div class=" Screen3 bounce-enter-active text-center">
+                        <h3>การเดิมพันทั้งหมด</h3>
                     </div>
                 </div>
             </div>
         </div>
-        <!--   <div class="row mt-4 justify-content-center">
-                        <div class="col-6 col-md-6 col-lg-3 mb-2">
-                            <button class="buttonB" id="redBtn"><img src="../assets/redb.png" width="100px"></button>
-                        </div>
-                        <div class="col-12 col-md-12 col-lg-6">
-                            <div class="row">
-                                <div class="gradientBtn">
-                                    <button class="gBtn">Text</button>
-                                </div>
-                            </div>
-                            <div class="row mt-3">
-                                <div class="col-12 col-md-6 col-lg-3 mb-2 d-flex justify-content-center">
-                                    <button class="btnF">btn</button>
-                                </div>
-                                <div class="col-12 col-md-12 col-lg-6 ">
-                                    <div class="form-outline">
-                                        <textarea class="form-control" id="textAreaExample1" rows="3" cols=""></textarea>
-                                        
-                                    </div>
-                                </div>
-                                <div class="col-12 col-md-6 col-lg-3 mt-2  d-flex justify-content-center">
-                                    <button class="btnF">btn</button>
-                                </div>
-                            </div>
-                            <div class="d-flex justify-content-between">
-                                <button class="btnp">+10</button>
-                                <button class="btnp" >+10</button>
-                                <button class="btnp">+10</button>
-                                <button class="btnp">+10</button>
-                                <button class="btnp">+10</button>
-                            </div>
-                        </div>
-                        <div class="col-6 col-md-6 col-lg-3 mt-3 mb-2">
-                            <button class="buttonB" id="blueBtn"><img src="../assets/blueb.png" width="100px"></button>
-                        </div>
-                    </div> -->
+       
     </div>
 
 
@@ -128,6 +93,7 @@ export default {
 <style scoped>
 .mainScreen {
     background-image: url('../assets/Background.png');
+    background-size: 100%;
 }
 
 .screen {
@@ -152,7 +118,7 @@ export default {
 
     border-image: linear-gradient(to right, #EE2623, #F78E1E);
     border-image-slice: 1;
-    border-radius: 5px;
+    border-radius: 20px !important;
 }
 
 .Screen3 {
@@ -183,9 +149,8 @@ export default {
     border: none;
 }
 
-.vs {
-    /* background-image: linear-gradient(to right, #C62D21 , #0436FA);
-    border-radius: 5px; */
+.vs img {
+   margin-left:110px
 }
 
 .gradientBtn {
@@ -225,6 +190,7 @@ export default {
     color:#000;
     width: 100px;
     margin-left: 3px;
+    font-weight: 700 !important;
     
 }
 
