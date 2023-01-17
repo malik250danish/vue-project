a19c9bb9cdedfc79a60aaa2eafc508fed56f459a
danish ahmad <danish250ahmad@gmail.com>
2023-01-17 16:48:16 +0500

    hhhgfh

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -9,8 +9,10 @@
         <div class="p1 d-flex flex-column  justify-content-between">
           <div class="">
             <label for="exampleInputEmail1" class="form-label"> เบอร์โทรศัพท์</label>
-            <div class="d-flex">
-              <i class="fa fa-phone me-4" style="font-size:36px"></i>
+            <div class="d-flex justify-conent-between">
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+</svg>
               <input type="text" class="form-control f-input " id="exampleInputEmail1" aria-describedby="emailHelp"
                 placeholder=" เบอร์โทรศัพท์">
             </div>
@@ -18,8 +20,10 @@
           </div>
           <div class="">
             <label for="exampleInputEmail1" class="form-label">ยืนยันรหัสผ่าน</label>
-            <div class="d-flex">
-              <i class="fas fa-key me-4" style="font-size:36px"></i>
+            <div class="d-flex justify-conent-between">
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+  <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+</svg>
               <input type="password" class="form-control f-input " id="exampleInputEmail1" aria-describedby="emailHelp"
                 placeholder="ยืนยันรหัสผ่าน">
             </div>
@@ -28,15 +32,19 @@
          
           <div class="">
             <label for="exampleInputEmail1" class="form-label">รหัสผ่าน</label>
-            <div class="d-flex">
-              <i class="fas fa-key me-4" style="font-size:36px"></i>
+            <div class="d-flex justify-conent-between">
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+  <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+</svg>
               <input type="text" class="form-control f-input " id="exampleInputEmail1" aria-describedby="emailHelp"
                 placeholder="รหัสผ่าน">
             </div>
             <div class="">
             <label for="exampleInputEmail1" class="form-label"> รหัสแนะนำ</label>
-            <div class="d-flex">
-              <i class="fa fa-user-friends me-4" style="font-size:36px"></i>
+            <div class="d-flex justify-conent-between">
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+  <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+</svg>
               <input type="text" class="form-control f-input " id="exampleInputEmail1" aria-describedby="emailHelp"
                 placeholder="รหัสแนะนำ">
             </div>
@@ -49,16 +57,20 @@
         <div class="p2 d-flex flex-column">
           <div class="">
               <label for="exampleInputEmail1" class="form-label">เลขที่บัญชีธนาคาร</label>
-              <div class="d-flex">
-                <i class="fas fa-credit-card me-4" style="font-size:36px"></i>
+              <div class="d-flex justify-conent-between">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-credit-card-fill" viewBox="0 0 16 16">
+  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v1H0V4zm0 3v5a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7H0zm3 2h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1z"/>
+</svg>
                 <input type="email" class="form-control f-input " id="exampleInputEmail1" aria-describedby="emailHelp"
                   placeholder="เลขที่บัญชีธนาคาร">
               </div>
             </div>
           <div class="">
             <label for="exampleInputEmail1" class="form-label"> บัญชีธนาคาร</label>
-            <div class="d-flex">
-              <i class="fas fa-credit-card me-4" style="font-size:36px"></i>
+            <div class="d-flex justify-conent-between">
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-credit-card-fill" viewBox="0 0 16 16">
+  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v1H0V4zm0 3v5a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7H0zm3 2h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1z"/>
+</svg>
               <input type="text" class="form-control f-input " id="exampleInputEmail1" aria-describedby="emailHelp"
                 placeholder=" บัญชีธนาคาร">
             </div>
@@ -67,8 +79,10 @@
           
           <div class="">
             <label for="exampleInputEmail1" class="form-label"> ชื่อบัญชี</label>
-            <div class="d-flex">
-              <i class="fa fa-user me-4" style="font-size:36px"></i>
+            <div class="d-flex justify-conent-between">
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+</svg>
               <input type="text" class="form-control f-input " id="exampleInputEmail1" aria-describedby="emailHelp"
                 placeholder=" ชื่อบัญชี">
             </div>
@@ -77,9 +91,9 @@
          
           <div class="">
             <label for="exampleInputEmail1" class="form-label">กรุณากรอกข้อมูล</label>
-            <div class="d-flex">
+            <div class="d-flex justify-conent-between">
              <span>    </span>
-              <input type="text" class="form-control f-input " id="exampleInputEmail1" aria-describedby="emailHelp"
+              <input type="text" class="form-control f-input " id="emp" aria-describedby="emailHelp"
                 placeholder="กรุณากรอกข้อมูล">
             </div>
           </div>
@@ -135,9 +149,12 @@ export default {
 
 .f-input {
   width: 300px;
-  height: 50px
+  height: 50px;
+  margin-left:15px
 }
-
+#emp{
+  margin-left:50px
+}
 .p1 {
   margin-right: 80px;
 }
